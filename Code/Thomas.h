b4f4f94e5ed1a67ff2c9fb86366493c0ad827929
Sahi12S <sahil.sharma@sjsu.edu>
2019-02-13 18:11:07 -0800

    Playable level & collision detection

---
+++ Code/Thomas.h
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "PlayableCharacter.h"
+
+class Thomas : public PlayableCharacter
+{
+public:
+	// A constructor specific to Thomas
+	Thomas();
+
+	// The overridden input handler for Thomas
+	bool virtual handleInput();
+};
